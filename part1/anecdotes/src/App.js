50e7852b5ccbe183f979316b819622cc5a8215a8
yii-meow <yiksoonchong2002@gmail.com>
2023-03-17 18:38:11 +0800

    fix typo

--- part1/anecdotes/src/App.js
+++ part1/anecdotes/src/App.js
@@ -6,17 +6,17 @@ const Button = ({ handleClick, text }) => {
   )
 }
 
-const Anectode = ({ selected, count }) => {
+const Anecdote = ({ selected, count }) => {
   return (
     <div>
-      <h1>Anectode of the day</h1>
+      <h1>Anecdote of the day</h1>
       {selected} <br />
       has {count} votes
     </div>
   )
 }
 
-const MaxVotedAnectode = ({ anecdotes }) => {
+const MaxVotedAnecdote = ({ anecdotes }) => {
   // find the max vote of the object array
   let maxVote = Math.max(...anecdotes.map(a => a.count))
 
@@ -24,32 +24,32 @@ const MaxVotedAnectode = ({ anecdotes }) => {
   let maxVotePosition = anecdotes.find(a => a.count === maxVote)
 
   return (<div>
-    <h1>Anectode with most votes</h1>
-    {maxVotePosition.anectode} <br /> has {maxVotePosition.count} votes
+    <h1>Anecdote with most votes</h1>
+    {maxVotePosition.anecdote} <br /> has {maxVotePosition.count} votes
   </div>);
 }
 
 const App = () => {
   const [anecdotes, setAnecdotes] = useState(
     [
-      { "anectode": 'If it hurts, do it more often.', count: 0 },
-      { "anectode": 'Adding manpower to a late software project makes it later!', count: 0 },
-      { "anectode": 'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.', count: 0 },
-      { "anectode": 'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.', count: 0 },
-      { "anectode": 'Premature optimization is the root of all evil.', count: 0 },
-      { "anectode": 'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.', count: 0 },
-      { "anectode": 'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.', count: 0 },
-      { "anectode": 'The only way to go fast, is to go well.', count: 0 }
+      { "anecdote": 'If it hurts, do it more often.', count: 0 },
+      { "anecdote": 'Adding manpower to a late software project makes it later!', count: 0 },
+      { "anecdote": 'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.', count: 0 },
+      { "anecdote": 'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.', count: 0 },
+      { "anecdote": 'Premature optimization is the root of all evil.', count: 0 },
+      { "anecdote": 'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.', count: 0 },
+      { "anecdote": 'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.', count: 0 },
+      { "anecdote": 'The only way to go fast, is to go well.', count: 0 }
     ], 0
   )
 
   const [selected, setSelected] = useState(0)
 
-  const randomAnectode = () => {
+  const randomAnecdote = () => {
     setSelected(Math.floor(Math.random() * anecdotes.length))
   }
 
-  const voteAnectode = () => {
+  const voteAnecdote = () => {
     const copy = [...anecdotes]
     copy[selected].count += 1
     setAnecdotes(copy)
@@ -57,12 +57,12 @@ const App = () => {
 
   return (
     <div>
-      <Anectode selected={anecdotes[selected].anectode} count={anecdotes[selected].count} />
+      <Anecdote selected={anecdotes[selected].anecdote} count={anecdotes[selected].count} />
 
-      <Button handleClick={voteAnectode} text="vote" />
-      <Button handleClick={randomAnectode} text="next anectode" />
+      <Button handleClick={voteAnecdote} text="Vote" />
+      <Button handleClick={randomAnecdote} text="Next Anecdote" />
 
-      <MaxVotedAnectode anecdotes={anecdotes} />
+      <MaxVotedAnecdote anecdotes={anecdotes} />
     </div>
   )
 }
